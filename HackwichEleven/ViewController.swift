26ee658211965ddba1b4cf5e26e1d570edf8cbd6
Marijoy Jatico <142898185+maripandari@users.noreply.github.com>
2023-11-16 13:22:52 -1000

    Completed Guess The Number Game Part 1

--- HackwichEleven/ViewController.swift
+++ HackwichEleven/ViewController.swift
@@ -8,12 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var slider: UISlider!
+    var currentvalue: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func myGuessButtonPressed(_ sender: Any) {
+        
+        let message = "The value is: \(currentvalue)"
+        
+        let alert = UIAlertController(title: "Hello World", message: message,  preferredStyle:.alert)
+                               
+                let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+                               
+                alert.addAction(action)
+                               
+                present(alert, animated: true, completion: nil)
+        
+        
+        
+        
+        
+    }
+    
+    @IBAction func sliderHasMoved(_ sender: Any) {
+        
+        print("The Value of the slider is:\(slider.value)")
+        currentvalue = Int((slider.value))
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
 
